3d6ff761cd68c99eb8abca48e02e424393241c76
iL3sor <92763293+iL3sor@users.noreply.github.com>
2022-01-19 16:58:43 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect(){
     try {
-        await mongoose.connect("mongodb+srv://il3sor:0823572165.mkqi2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        await mongoose.connect("mongodb+srv://il3sor:0823572165@cluster0.mkqi2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
         console.log('successful connect')
     } catch (error) {
         console.log('failed')
